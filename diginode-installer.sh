57d3b8c8400714cad2983e885daf33960d703d7b
saltedlolly <olly@stedall.com>
2021-10-18 19:15:35 +0100

    Fix create/switch user whiptail dialogs

--- diginode-installer.sh
+++ diginode-installer.sh
@@ -1826,18 +1826,14 @@ if [[ "$USER_ASK_SWITCH" = "YES" ]]; then
     # Only ask to change the user if DigiByte Core is not yet installed
     if [ ! -f "$DGB_INSTALL_FOLDER/.officialdiginode" ]; then
 
-    if whiptail  --backtitle "" --title "Switching to the 'digibyte' user account is recommended." --yesno "\\n\\nIt is recommended that you switch to the 'digibyte' use account before running this installer. This is optional but recommended, since it will isolate your DigiByte wallet etc. its own user account. For more information visit:\\n  
-      $DGBH_URL_USERCHANGE\\n
+      if whiptail  --backtitle "" --title "Installing as user 'digibyte' is recommended." --yesno "It is recommended that you login as 'digibyte' before installing your DigiNode.\\n\\nThis is optional but encouraged, since it will isolate your DigiByte wallet its own user account.\\n\\nFor more information visit:\\n  $DGBH_URL_USERCHANGE\\n\\n\\nThere is already a 'digibyte' user account on this machine, but you are not currently using it - you are signed in as '$USER_CURRENT'. Would you like to switch users now?\\n\\nChoose NO to continue installation as '$USER_CURRENT'.\\n\\nChoose YES to exit and login as 'digibyte' from where you can run this installer again."  --yes-button "Yes (Recommended)" --no-button "No" "${r}" "${c}"; then
 
-Would you like to switch to the 'digibyte' user now?
-
-Choose NO to continue installation as the current user."  --yes-button "Switch to 'digibyte' (Recommended)" --no-button "Continue as '$USER_CURRENT'" "${r}" "${c}"; then
-
-      USER_DO_SWITCH="YES"
-        user_do_change
-    else
-      printf "%b Current user '$USER_CURRENT' will be used for the installation.\\n" "${INFO}"
-    fi
+        USER_DO_SWITCH="YES"
+          user_do_change
+      else
+        printf "%b Current user '$USER_CURRENT' will be used for the installation.\\n" "${INFO}"
+      fi
+  fi
 fi
 
 # Display a request to create the 'digibyte' user, if needed
@@ -1846,19 +1842,15 @@ if [[ "$USER_ASK_CREATE" = "YES" ]]; then
     # Only ask to create the user if DigiByte Core is not yet installed
     if [ ! -f "$DGB_INSTALL_FOLDER/.officialdiginode" ]; then
 
-    if whiptail  --backtitle "" --title "Creating a 'digibyte' user account is recommended." --yesno "\\n\\nIt is recommended that you create a 'digibyte' use account to run your DigiNode. This is optional but recommended, since it will isolate your DigiByte wallet etc. its own user account. For more information visit:\\n  
-      $DGBH_URL_USERCHANGE\\n
+      if whiptail  --backtitle "" --title "Creating a new 'digibyte' user is recommended." --yesno "It is recommended that you create a new 'digibyte' user for your DigiNode.\\n\\nThis is optional but encouraged, since it will isolate your DigiByte wallet in its own user account.\\n\\nFor more information visit:\\n$DGBH_URL_USERCHANGE\\n\\n\\nYou are currently signed in as user '$USER_CURRENT'. Would you like to create a new 'digibyte' user now?\\n\\nChoose YES to create and sign in to the new user account, from where you can run this installer again.\\n\\nChoose NO to continue installation as '$USER_CURRENT'."  --yes-button "Yes (Recommended)" --no-button "No" "${r}" "${c}"; then
 
-Would you like to create 'digibyte' user now?
-
-Choose NO to continue installation as the current user."  --yes-button "Create 'digibyte' user (Recommended)" --no-button "Continue as '$USER_CURRENT'" "${r}" "${c}"; then
-
-      USER_DO_SWITCH="YES"
-      printf "%b User selected option to create 'digibyte' user.\\n" "${INFO}"
-      user_do_change
-    else
-      printf "%b Current user '$USER_CURRENT' will be used for the DigiNode.\\n" "${INFO}"
-    fi
+        USER_DO_SWITCH="YES"
+        printf "%b User selected option to create 'digibyte' user.\\n" "${INFO}"
+        user_do_change
+      else
+        printf "%b Current user ''$USER_CURRENT' will be used for the DigiNode.\\n" "${INFO}"
+      fi
+  fi
 fi
 
 }
