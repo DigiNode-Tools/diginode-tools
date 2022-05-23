16fbc51aa94257d0c8065154bedbecd754e178e7
saltedlolly <olly@stedall.com>
2022-05-23 17:44:31 +0100

    Update backup intro text

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -2987,7 +2987,7 @@ usb_backup() {
 
 
         # Introduction to backup.
-        if whiptail --backtitle "" --title "DigiNode Backup" "This tool will help you to backup your DigiByte Core wallet and/or DigiAsset Node settings. You will need a USB stick for this. It is highly reccomended that this stick not be used for anything else - you should backup your DigiNode to it and then place it somewhere secure, such as a safe. You do not need a large capacity USB stick for this - pretty much any size stick should be sufficient. For best results, make sure it is formatted with either exFAT or FAT32.\\n\\nIMPORTANT: You will also need direct access to a free USB slot on your DigiNode." --yesno --yes-button "Continue" --no-button "Exit" "${r}" "${c}"; then
+        if whiptail --backtitle "" --title "DigiNode Backup" "This tool will help you to backup your DigiByte Core wallet and/or DigiAsset Node settings to a USB stick.\\n\\nIt is highly reccomended that you use a USB stick that is not used for anything else - you should backup your DigiNode to it and then place it somewhere secure, such as a safe. You do not need a large capacity USB stick for this - pretty much any size stick should be sufficient. For best results, make sure it is formatted with either exFAT or FAT32.\\n\\nIMPORTANT: You will also need access to a free USB slot on your DigiNode." --yesno --yes-button "Continue" --no-button "Exit" "${r}" "${c}"; then
 
             printf "%b You chose to begin the backup process.\\n" "${INFO}"
         else
@@ -3226,9 +3226,9 @@ usb_backup() {
         # Mount USB stick
         local format_usb_stick_now=false
         printf "%b Trying to mount USB...\\n" "${INFO}"
-        if [ -d "/dev/${USB_BACKUP_DRIVE}2" ]; then
+        if [ -f "/dev/${USB_BACKUP_DRIVE}2" ]; then
             mount /dev/${USB_BACKUP_DRIVE}2 /media/usbbackup 1>/dev/null
-        elif [ -d "/dev/${USB_BACKUP_DRIVE}1" ]; then
+        elif [ -f "/dev/${USB_BACKUP_DRIVE}1" ]; then
             mount /dev/${USB_BACKUP_DRIVE}1 /media/usbbackup 1>/dev/null
         else
             rmdir /media/usbbackup
