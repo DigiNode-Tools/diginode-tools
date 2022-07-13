681d9d1171049e20ae4350735e69f2a4ef5ed2d7
saltedlolly <olly@stedall.com>
2022-07-13 22:29:53 +0100

    v0.3.7 Test creating new diginode.settings.new file

--- diginode.sh
+++ diginode.sh
@@ -54,8 +54,8 @@
 # When a new release is made, this number gets updated to match the release number on GitHub.
 # The version number should be three numbers seperated by a period
 # Do not change this number or the mechanism for installing updates may no longer work.
-DGNT_VER_LOCAL=0.3.6
-# Last Updated: 2022-07-11
+DGNT_VER_LOCAL=0.3.7
+# Last Updated: 2022-07-13
 
 # This is the command people will enter to run the install script.
 DGNT_SETUP_OFFICIAL_CMD="curl -sSL diginode-setup.digibyte.help | bash"
