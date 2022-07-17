4fd704ead24fe500f18869d1b158c6985776be49
saltedlolly <olly@stedall.com>
2022-07-17 22:42:33 +0100

    Format block count in Status Monitor with commas

--- diginode.sh
+++ diginode.sh
@@ -1137,6 +1137,7 @@ pre_loop() {
       DGB_STATUS="startingup"
     else
       BLOCKCOUNT_LOCAL=$BLOCKCOUNT_LOCAL_QUERY
+      BLOCKCOUNT_FORMATTED=$(printf "%'d" $BLOCKCOUNT_LOCAL)
 
       # Query current version number of DigiByte Core
       DGB_VER_LOCAL_QUERY=$($DGB_CLI getnetworkinfo 2>/dev/null | grep subversion | cut -d ':' -f3 | cut -d '/' -f1)
@@ -1346,6 +1347,7 @@ if [ "$DGB_STATUS" = "running" ]; then
     # If the blockchain is not yet synced, get blockcount
     if [ "$BLOCKSYNC_PROGRESS" = "" ] || [ "$BLOCKSYNC_PROGRESS" = "notsynced" ]; then
       BLOCKCOUNT_LOCAL=$($DGB_CLI getblockcount 2>/dev/null)
+      BLOCKCOUNT_FORMATTED=$(printf "%'d" $BLOCKCOUNT_LOCAL)
 
       # If we don't get a response, assume it is starting up
       if [ "$BLOCKCOUNT_LOCAL" = "" ]; then
@@ -1433,10 +1435,13 @@ if [ $TIME_DIF_15SEC -ge 15 ]; then
     # Is digibyted in the process of starting up, and not ready to respond to requests?
     if [ "$DGB_STATUS" = "running" ] && [ "$BLOCKSYNC_PROGRESS" = "synced" ]; then
         BLOCKCOUNT_LOCAL=$($DGB_CLI getblockcount 2>/dev/null)
+        BLOCKCOUNT_FORMATTED=$(printf "%'d" $BLOCKCOUNT_LOCAL)
         if [ "$BLOCKCOUNT_LOCAL" = "" ]; then
           DGB_STATUS="startingup"
         fi
     fi
+
+
 
     # If there is a new DigiByte Core release available, check every 15 seconds until it has been installed
     if [ $DGB_STATUS = "running" ]; then
@@ -1802,7 +1807,7 @@ else
 printf "  ║ CONNECTIONS   ║  " && printf "%-10s %35s %-4s\n" "$DGB_CONNECTIONS Nodes" "[ $DGB_CONNECTIONS_MSG" "]  ║"
 fi
 printf "  ╠═══════════════╬════════════════════════════════════════════════════╣\\n"
-printf "  ║ BLOCK HEIGHT  ║  " && printf "%-26s %19s %-4s\n" "$BLOCKCOUNT_LOCAL Blocks" "[ Synced: $BLOCKSYNC_PERC%" "]  ║"
+printf "  ║ BLOCK HEIGHT  ║  " && printf "%-26s %19s %-4s\n" "$BLOCKCOUNT_FORMATTED Blocks" "[ Synced: $BLOCKSYNC_PERC%" "]  ║"
 printf "  ╠═══════════════╬════════════════════════════════════════════════════╣\\n"
 printf "  ║ NODE UPTIME   ║  " && printf "%-49s ║ \n" "$uptime"
 printf "  ╠═══════════════╬════════════════════════════════════════════════════╣\\n"
