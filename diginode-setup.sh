8b16480ec5e27adbf802734396475a648ce8da21
saltedlolly <olly@stedall.com>
2022-08-10 17:10:48 +0100

    Update diginode-setup.sh

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -2924,8 +2924,8 @@ swap_check() {
     # These check for a minimum total of 12Gb, but recommend a total of 16Gb. THe user can then choose how big they want.
     #
 
-    echo: "RAMTOTAL_KB: $RAMTOTAL_KB"
-    echo: "SWAPTOTAL_KB: $SWAPTOTAL_KB"
+    echo "RAMTOTAL_KB: $RAMTOTAL_KB"
+    echo "SWAPTOTAL_KB: $SWAPTOTAL_KB"
 
     if [ "$RAMTOTAL_KB" -le "1000000" ] && [ "$SWAPTOTAL_KB" -gt "0" ] && [ "$SWAPTOTAL_KB" -le "10742187" ];  then
         SWAP_TOO_SMALL="YES"
