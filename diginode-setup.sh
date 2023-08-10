8c91f579e3f49340fac1205464bb4684b5da06e6
saltedlolly <olly@stedall.com>
2023-08-10 17:42:02 +0100

    Rename 8.22.0-rc2 download folder because it is non standard

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -8046,6 +8046,14 @@ if [ "$DGB_DO_INSTALL" = "YES" ]; then
         printf "%b%b %s Done!\\n" "${OVER}" "${TICK}" "${str}"
     fi
 
+    # If this is 8.22.0-rc2, we need to manually rename the extracted directory name, since it is incorrect
+    if [ -d "$USER_HOME/digibyte-af42429717ac" ] && [ ${DGB_VER_RELEASE} = "8.22.0-rc2 "]; then
+        str="Renaming 8.22.0-rc2 download folder..."
+        printf "%b %s" "${INFO}" "${str}"
+        sudo -u $USER_ACCOUNT mv $USER_HOME/digibyte-af42429717ac $USER_HOME/digibyte-8.22.0-rc2
+        printf "%b%b %s Done!\\n" "${OVER}" "${TICK}" "${str}"
+    fi
+
     # Delete old ~/digibyte symbolic link
     if [ -h "$USER_HOME/digibyte" ]; then
         str="Deleting old 'digibyte' symbolic link from home folder..."
