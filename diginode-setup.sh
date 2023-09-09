43b72a66af00fcc179f5b6a4a874356f87036b9f
saltedlolly <olly@stedall.com>
2023-09-09 09:07:32 +0100

    bug

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -13132,7 +13132,7 @@ uninstall_do_now() {
  #                   if whiptail --backtitle "" --title "UNINSTALL" --yesno "Would you like to also delete your Node.js settings folder?\\n\\nThis will delete the folder: ~/.npm\\n\\nThis folder contains all the settings related to the Node package manager." "${r}" "${c}"; then
                         str="Deleting ~/.npm settings folder..."
                         printf "%b %s" "${INFO}" "${str}"
-                        rm -r $USER_HOME/.rpm
+                        rm -r $USER_HOME/.npm
                         printf "%b%b %s Done!\\n" "${OVER}" "${TICK}" "${str}"
 #                    else
 #                        printf "%b You chose not to delete the Node.js settings folder (~/.npm).\\n" "${INFO}"
