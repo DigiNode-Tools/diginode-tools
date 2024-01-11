62878c3ec2584dfab2ea57bdeebfdde9980555b0
saltedlolly <olly@stedall.com>
2024-01-11 11:25:00 +0000

    Update diginode-setup.sh

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -6500,6 +6500,8 @@ fi
 
 }
 
+# 3>&1 1>&2 2>&1
+
 # The menu displayed on first install - asks to install DigiByte Core alone, or also the DigiAsset Node
 menu_first_install() {
 
@@ -6516,10 +6518,10 @@ menu_first_install() {
     opt3b="Use DigiNode Dashboard with an existing DigiByte Node."
 
     # Display the information to the user
-    UpdateCmd=$(dialog --no-shadow --keep-tite --colors --backtitle "DigiNode Setup - Main Menu" --title "DigiNode Setup - Main Menu" --cancel-label "Exit" --menu "\nPlease choose what to install. A FULL DigiNode is recommended.\n\nRunning a DigiAsset Node supports the DigiByte network by helping to decentralize DigiAsset metadata. You can also use it to mint your own DigiAssets and earn \$DGB for hosting the community metadata.\n\nIf you already have a DigiByte Node on this machine, you can install DigiNode Tools ONLY to use the DigiNode Dashboard with it. Note: This may require you to tweak your setup to work.\n\nPlease choose an option:\n\n" 22 "${c}" 4 \
+    UpdateCmd=$(dialog --no-shadow --keep-tite --colors --backtitle "DigiNode Setup - Main Menu" --title "DigiNode Setup - Main Menu" --cancel-label "Exit" --menu "\nPlease choose what to install. A FULL DigiNode is recommended.\n\nRunning a DigiAsset Node supports the DigiByte network by helping to decentralize DigiAsset metadata. You can also use it to mint your own DigiAssets and earn \$DGB for hosting the community metadata.\n\nIf you already have a DigiByte Node on this machine, you can install DigiNode Tools ONLY to use the DigiNode Dashboard with it. Note: This may require you to tweak your setup to work.\n\nPlease choose an option:\n\n" 22 75 4 \
         "${opt1a}" "${opt1b}" \
         "${opt2a}" "${opt2b}" \
-        "${opt3a}" "${opt3b}" 3>&1 1>&2 2>&1) || \
+        "${opt3a}" "${opt3b}" ) || \
     { printf "%b %bExit was selected.%b\n" "${INDENT}" "${COL_LIGHT_RED}" "${COL_NC}"; printf "\n"; digifact_randomize; display_digifact_fixedwidth; printf "\n"; exit; }
 
     # Set the variable based on if the user chooses
