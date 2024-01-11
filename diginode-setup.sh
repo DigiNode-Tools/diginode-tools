739108b804fddd2d026796d17b75c2e69d9738ea
saltedlolly <olly@stedall.com>
2024-01-11 13:07:39 +0000

    Update diginode-setup.sh

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -6506,14 +6506,14 @@ menu_first_install() {
     printf " =============== INSTALL MENU ==========================================\n\n"
     # ==============================================================================
 
-    opt1="1"
-    desc1="FULL DigiNode: Install DigiByte & DigiAsset Node (Recommended)"
+    opt1="1 FULL DigiNode"
+    desc1="Install DigiByte & DigiAsset Node (Recommended)"
     
-    opt2="2"
-    desc2="DigiByte Node: Install DigiByte Node ONLY."
+    opt2="2 DigiByte Node"
+    desc2="Install DigiByte Node ONLY."
 
-    opt3="3"
-    desc3="DigiNode Tools ONLY: Use DigiNode Dashboard with an existing DigiByte Node."
+    opt3="3 DigiNode Tools ONLY"
+    desc3="Use DigiNode Dashboard with an existing DigiByte Node."
 
     # Display the information to the user
     UpdateCmd=$(dialog --no-shadow --keep-tite --colors --backtitle "DigiNode Setup - Main Menu" --title "DigiNode Setup - Main Menu" --cancel-label "Exit" --menu "\nPlease choose what to install. A FULL DigiNode is recommended.\n\nRunning a DigiAsset Node supports the DigiByte network by helping to decentralize DigiAsset metadata. You can also use it to mint your own DigiAssets and earn \$DGB for hosting the community metadata.\n\nIf you already have a DigiByte Node on this machine, you can install DigiNode Tools ONLY to use the DigiNode Dashboard with it. Note: This may require you to tweak your setup to work.\n\nPlease choose an option:\n\n" 22 84 3 \
