c04633938002493a083e50ebc9d8a42f9874e3d9
saltedlolly <olly@stedall.com>
2024-02-03 23:23:13 +0000

    v0.9.8 Prep

--- diginode-setup.sh
+++ diginode-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#           Name:  DigiNode Setup v0.9.7
+#           Name:  DigiNode Setup v0.9.8
 #
 #        Purpose:  Install and manage a DigiByte Node and DigiAsset Node via the linux command line.
 #          
