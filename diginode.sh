d92bcec9213dcfca720939fa7ed1059e3c693e88
saltedlolly <olly@stedall.com>
2024-04-03 00:12:44 +0100

    v0.9.12 Prep

--- diginode.sh
+++ diginode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#           Name:  DigiNode Dashboard v0.9.11
+#           Name:  DigiNode Dashboard v0.9.12
 #
 #        Purpose:  Monitor and manage the status of you DigiByte Node and DigiAsset Node.
 #          
@@ -60,8 +60,8 @@
 # Whenever there is a new release, this number gets updated to match the release number on GitHub.
 # The version number should be three numbers seperated by a period
 # Do not change this number or the mechanism for installing updates may no longer work.
-DGNT_VER_LOCAL=0.9.11
-# Last Updated: 2024-03-29
+DGNT_VER_LOCAL=0.9.12
+# Last Updated: 2024-04-03
 
 # This is the command people will enter to run the install script.
 DGNT_SETUP_OFFICIAL_CMD="curl -sSL setup.diginode.tools | bash"
